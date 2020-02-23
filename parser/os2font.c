0cdafac94bc278c91500a302bd7f10256f8c7bc7
Bastian Maerkisch <bmaerkisch@web.de>
2020-02-23 21:14:16 +0100

    Extend to show info about all fonts in a file
    If no glyph index is given on the command line, we iterate over all
    fonts in a file.

--- parser/os2font.c
+++ parser/os2font.c
@@ -54,6 +54,7 @@ int main( int argc, char *argv[] )
                     cb,
                     error;
     PBYTE           pBuf;
+    int             n;
 
     if ( argc < 2 ) {
         printf("Syntax: TEST <FNT filename> [Unicode index]\n");
@@ -63,69 +64,77 @@ int main( int argc, char *argv[] )
     if (( argc < 3 ) || ( !sscanf( argv[2], "%x", &index )))
         index = 0;
 
+    n = 0;
+    do { // loop
 
-    error = ReadOS2FontResource( pszFile, 0, &total, &font );
-    if ( error ) {
-        switch ( error ) {
-            case ERR_FILE_OPEN:
-                printf("The file %s could not be opened.\n", pszFile );
-                break;
-            case ERR_FILE_STAT:
-            case ERR_FILE_READ:
-                printf("Failed to read file %s.\n", pszFile );
-                break;
-            case ERR_FILE_FORMAT:
-                printf("The file %s does not contain a valid font.\n", pszFile );
-                break;
-            case ERR_NO_FONT:
-                printf("The requested font number was not found in %s\n", pszFile );
-                break;
-            case ERR_MEMORY:
-                printf("A memory allocation error occurred.\n");
+        error = ReadOS2FontResource( pszFile, n, &total, &font );
+        if ( error ) {
+            switch ( error ) {
+                case ERR_FILE_OPEN:
+                    printf("The file %s could not be opened.\n", pszFile );
+                    break;
+                case ERR_FILE_STAT:
+                case ERR_FILE_READ:
+                    printf("Failed to read file %s.\n", pszFile );
+                    break;
+                case ERR_FILE_FORMAT:
+                    printf("The file %s does not contain a valid font.\n", pszFile );
+                    break;
+                case ERR_NO_FONT:
+                    printf("The requested font number was not found in %s\n", pszFile );
+                    break;
+                case ERR_MEMORY:
+                    printf("A memory allocation error occurred.\n");
+                    break;
+                default:
+                    printf("An unknown error occurred.\n");
+                    break;
+            }
+            return error;
+        }
+
+        printf("File %s contains %u fonts.  Reading font %i.\n", pszFile, total, n );
+        printf(" - Font signature:    %s\n", font.pSignature->achSignature );
+        printf(" - Family name:       %s\n - Face name:         %s\n",
+               font.pMetrics->szFamilyname, font.pMetrics->szFacename );
+        if ( font.pMetrics->usKerningPairs )
+            printf(" - %d kerning pairs are defined\n", font.pMetrics->usKerningPairs );
+        if ( font.pPanose )
+            printf(" - PANOSE table:      (%u,%u,%u,%u,%u,%u,%u,%u,%u,%u)\n",
+                   font.pPanose->panose[0], font.pPanose->panose[1],
+                   font.pPanose->panose[2], font.pPanose->panose[3],
+                   font.pPanose->panose[4], font.pPanose->panose[5],
+                   font.pPanose->panose[6], font.pPanose->panose[7],
+                   font.pPanose->panose[8], font.pPanose->panose[8] );
+        printf(" - Font type:         ");
+        switch ( font.pFontDef->fsChardef ) {
+            case OS2FONTDEF_CHAR3:
+                printf("3 (ABC-space proportional-width)\n");
                 break;
             default:
-                printf("An unknown error occurred.\n");
+                if ( font.pFontDef->fsFontdef == OS2FONTDEF_FONT2 )
+                    printf("2 (single-increment proportional-width)\n");
+                else
+                    printf("1 (fixed-width)\n");
                 break;
         }
-        return error;
-    }
 
-    printf("File %s contains %u fonts.  Reading first font.\n", pszFile, total );
-    printf(" - Font signature:    %s\n", font.pSignature->achSignature );
-    printf(" - Family name:       %s\n - Face name:         %s\n",
-           font.pMetrics->szFamilyname, font.pMetrics->szFacename );
-    if ( font.pMetrics->usKerningPairs )
-        printf(" - %d kerning pairs are defined\n", font.pMetrics->usKerningPairs );
-    if ( font.pPanose )
-        printf(" - PANOSE table:      (%u,%u,%u,%u,%u,%u,%u,%u,%u,%u)\n",
-               font.pPanose->panose[0], font.pPanose->panose[1],
-               font.pPanose->panose[2], font.pPanose->panose[3],
-               font.pPanose->panose[4], font.pPanose->panose[5],
-               font.pPanose->panose[6], font.pPanose->panose[7],
-               font.pPanose->panose[8], font.pPanose->panose[8] );
-    printf(" - Font type:         ");
-    switch ( font.pFontDef->fsChardef ) {
-        case OS2FONTDEF_CHAR3:
-            printf("3 (ABC-space proportional-width)\n");
-            break;
-        default:
-            if ( font.pFontDef->fsFontdef == OS2FONTDEF_FONT2 )
-                printf("2 (single-increment proportional-width)\n");
-            else
-                printf("1 (fixed-width)\n");
-            break;
-    }
+        printf(" - Point size:        %u (%ux%u dpi)\n",
+               font.pMetrics->usNominalPointSize / 10,
+               font.pMetrics->xDeviceRes, font.pMetrics->yDeviceRes );
+        printf(" - First glyph index: %u\n", font.pMetrics->usFirstChar );
+        printf(" - Last glyph index:  %u\n", font.pMetrics->usLastChar + font.pMetrics->usFirstChar );
+        printf(" - Default character: %u\n", font.pMetrics->usDefaultChar + font.pMetrics->usFirstChar );
+        printf(" - Break character:   %u\n", font.pMetrics->usBreakChar + font.pMetrics->usFirstChar );
 
-    printf(" - Point size:        %u (%ux%u dpi)\n",
-           font.pMetrics->usNominalPointSize / 10,
-           font.pMetrics->xDeviceRes, font.pMetrics->yDeviceRes );
-    printf(" - First glyph index: %u\n", font.pMetrics->usFirstChar );
-    printf(" - Last glyph index:  %u\n", font.pMetrics->usLastChar + font.pMetrics->usFirstChar );
-    printf(" - Default character: %u\n", font.pMetrics->usDefaultChar + font.pMetrics->usFirstChar );
-    printf(" - Break character:   %u\n", font.pMetrics->usBreakChar + font.pMetrics->usFirstChar );
+        printf("\n");
+
+        if ( index != 0 ) break;
+
+    } while ( ++n < total );
+
     if ( index == 0 ) return 0;
 
-    printf("\n");
     offset = OS2FontGlyphIndex( &font, index );
     if ( offset == 0 )
         printf("The character U+%04X is not supported by this font. Loading default glyph %u.\n",
